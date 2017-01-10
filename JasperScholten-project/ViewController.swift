4a946aa375fd2e67a4b971a7f609a41d8a2a5337
jasperscholten <j.o.scholten@student.vu.nl>
2017-01-10 09:50:03 +0100

    Initial Commit

---
+++ JasperScholten-project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JasperScholten-project
+//
+//  Created by Jasper Scholten on 10-01-17.
+//  Copyright © 2017 Jasper Scholten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
