da57640148ffe67fa3861348787134662dd6f2c4
jasperscholten <j.o.scholten@student.vu.nl>
2017-01-12 13:25:04 +0100

    Name change

---
+++ JasperScholten-project/ReviewResultFormVC.swift
@@ -0,0 +1,45 @@
+//
+//  ReviewResultFormVC.swift
+//  JasperScholten-project
+//
+//  Created by Jasper Scholten on 12-01-17.
+//  Copyright © 2017 Jasper Scholten. All rights reserved.
+//
+
+import UIKit
+
+class ReviewResultFormVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var reviewFormTableView: UITableView!
+    @IBOutlet weak var commentField: UITextView!
+    
+    let questions = ["Klanten worden door de medewerker opgemerkt.", "Klanten worden door de medewerker begroet.", "De medewerker spreekt de klanten aan.", "De medewerker opent het gesprek.", "De medewerker toont de geadviseerde producten.", "De medewerker heeft gevraagd of hij de klant nog ergens anders mee kan helpen.", "De medewerker heeft afscheid genomen van de klant."]
+    let result = [true, true, true, true, false, false, true]
+    let comment = "De basis staat, nu nog verderwerken aan de details."
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        commentField.text = comment
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = reviewFormTableView.dequeueReusableCell(withIdentifier: "reviewFormCell", for: indexPath) as! ReviewResultFormCell
+        
+        cell.questionField.text = questions[indexPath.row]
+        cell.answerSwitch.isOn = result[indexPath.row]
+        
+        return cell
+    }
+
+}
---
+++ JasperScholten-project/ReviewResultsVC.swift
@@ -0,0 +1,54 @@
+//
+//  ReviewResultsVC.swift
+//  JasperScholten-project
+//
+//  Created by Jasper Scholten on 12-01-17.
+//  Copyright © 2017 Jasper Scholten. All rights reserved.
+//
+
+import UIKit
+
+class ReviewResultsVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var resultListTableView: UITableView!
+    
+    let employees = ["Hans Beerekamp", "Ineke Bosch", "Niels Pel", "Marinus Zeekoe", "Emma Post", "Henk van Ingrid", "Ingrid van Henk", "Jan Janssen"]
+    let results = ["9.0", "7.5", "6.5", "8.0", "7.0", "8.0", "7.5", "9.0"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return employees.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = resultListTableView.dequeueReusableCell(withIdentifier: "resultListID", for: indexPath) as! ReviewResultsCell
+        
+        cell.employeeName.text = employees[indexPath.row]
+        cell.employeeResult.text = results[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showReviewsEmployee", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let employeeResults = segue.destination as? ReviewResultsEmployeeVC {
+            let indexPath = self.resultListTableView.indexPathForSelectedRow
+            employeeResults.employee = employees[indexPath!.row]
+        }
+    }
+
+}
