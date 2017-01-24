51312a46ddccc3c200cc00d2530c34eb88244d9c
jasperscholten <j.o.scholten@student.vu.nl>
2017-01-24 14:03:59 +0100

    Accepteren kan nu, waarbij gekozen kan worden uit de beschikbare locaties.

--- JasperScholten-project/AddEmployeeVC.swift
+++ JasperScholten-project/AddEmployeeVC.swift
@@ -9,19 +9,22 @@
 import UIKit
 import Firebase
 
-class AddEmployeeVC: UIViewController {
+class AddEmployeeVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     // MARK: Constants and variables
     let ref = FIRDatabase.database().reference(withPath: "Users")
+    let locationsRef = FIRDatabase.database().reference(withPath: "Locations")
     var user = User(uid: "", email: "", name: "", admin: false, employeeNr: "", organisationName: "", organisationID: "", locationID: "", accepted: false, key: "")
-    var organisation = String()
+    var organisationID = String()
+    var selectedLocation = String()
+    var locations = [String]()
     
     // MARK: - Outlets
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var mail: UILabel!
     @IBOutlet weak var employee: UITextField!
-    @IBOutlet weak var location: UITextField!
     @IBOutlet weak var role: UISegmentedControl!
+    @IBOutlet weak var location: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,14 +38,51 @@ class AddEmployeeVC: UIViewController {
             for item in snapshot.children {
                 let userData = User(snapshot: item as! FIRDataSnapshot)
                 if userData.uid == (FIRAuth.auth()?.currentUser?.uid)! {
-                    self.organisation = userData.organisationID!
+                    self.organisationID = userData.organisationID!
                 }
+            }
+        })
+        
+        locationsRef.observe(.value, with: { snapshot in
+            
+            let locationSnapshot = snapshot.childSnapshot(forPath: self.organisationID)
+            let locationData = locationSnapshot.value as! [String: String]
+            
+            var newLocations: [String] = []
+            for (_, value) in locationData {
+                newLocations.append(value)
+            }
+            self.locations = newLocations
+            self.location.reloadAllComponents()
+            
+            print(self.locations)
+            var locationNumber = 0
+            for selected in self.locations {
+                print(selected)
+                if selected == self.user.locationID {
+                    self.location.selectRow(locationNumber, inComponent: 0, animated: true)
+                }
+                locationNumber += 1
             }
         })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Picker views
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return locations.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return locations[row]
     }
     
     // MARK: - Actions
@@ -61,7 +101,7 @@ class AddEmployeeVC: UIViewController {
                                                "admin":admin,
                                                "employeeNr":employee.text!,
                                                "organisationID":user.organisationID!,
-                                               "locationID":user.locationID!,
+                                               "locationID":locations[self.location.selectedRow(inComponent: 0)],
                                                "accepted":true])
         
         _ = navigationController?.popViewController(animated: true)
